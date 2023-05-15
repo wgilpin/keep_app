fe75c80b74a1ee3dbfa44049491afc5d874d99e9
Will Gilpin <wgilpin@gmail.com>
2023-05-15 17:53:01 +0100

    remove test code

--- lib/src/views/profile.dart
+++ lib/src/views/profile.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:keep_app/src/views/recommend.dart';
+import 'package:get/get.dart';
+import 'package:keep_app/src/views/home_page.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final r = Recommender();
-    r.testSearch('hello to the lords and ladoes of the revolution', 10, context);
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('rKyv'),
       ),
       body: const Center(
@@ -17,6 +17,27 @@ class Profile extends StatelessWidget {
           'Profile Page',
           style: TextStyle(fontSize: 24),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.yellow[700],
+        selectedItemColor: Colors.brown[900],
+        unselectedItemColor: Colors.grey[500],
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: 1,
+        onTap: (index) {
+          if (index == 0) {
+            Get.to(HomePage());
+          }
+        },
       ),
     );
   }
