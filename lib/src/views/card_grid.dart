1aa6b8e7ab09a2ab78af48cff744c782bb3557a5
Will Gilpin <wgilpin@gmail.com>
2023-05-15 23:14:13 +0100

    make number of columns on the grid responsive

--- lib/src/views/card_grid.dart
+++ lib/src/views/card_grid.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
@@ -18,9 +20,12 @@ class CardGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData media = MediaQuery.of(context);
+    int numColumns = max(1, media.size.width / 300 ~/ 1);
+    debugPrint("numColumns: $numColumns");
     return Scaffold(
       body: MasonryGridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: numColumns,
         mainAxisSpacing: 4,
         crossAxisSpacing: 4,
         itemCount: _notes.length,
