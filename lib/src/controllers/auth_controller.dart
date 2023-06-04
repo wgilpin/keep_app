8e2ebbc692220398c05fbf292d6cf053ee4d30c2
Will Gilpin <wgilpin@gmail.com>
2023-06-04 23:23:12 +0100

    remove unused import

--- lib/src/controllers/auth_controller.dart
+++ lib/src/controllers/auth_controller.dart
@@ -2,8 +2,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-import '../views/login/login_page.dart';
-
 /// Controller for authentication
 class AuthCtl extends GetxController {
   final FirebaseAuth auth;
