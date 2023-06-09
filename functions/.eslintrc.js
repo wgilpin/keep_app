c59f063770f7433ba79fc1639d224aaed1a9572b
Will Gilpin <wgilpin@gmail.com>
2023-06-09 23:10:05 +0100

    eslint: prefer single quotes

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,20 +5,17 @@ module.exports = {
     jest: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ['eslint:recommended', 'google'],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    'no-restricted-globals': ['error', 'name', 'length'],
+    'prefer-arrow-callback': 'error',
+    'quotes': ['error', 'single', {allowTemplateLiterals: true}],
   },
   overrides: [
     {
-      files: ["**/*.spec.*"],
+      files: ['**/*.spec.*'],
       env: {
         mocha: true,
       },
