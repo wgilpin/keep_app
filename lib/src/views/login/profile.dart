dff463b5fba807a5db03225b23c259b04b2c60bf
Will Gilpin <wgilpin@gmail.com>
2023-06-11 17:40:14 +0100

    allow password reset

--- lib/src/views/login/profile.dart
+++ lib/src/views/login/profile.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:keep_app/src/controllers/auth_controller.dart';
-import 'package:keep_app/src/utils/utils.dart';
 import 'package:keep_app/src/views/home_page.dart';
 
 class Profile extends StatelessWidget {
@@ -23,16 +24,24 @@ class Profile extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            addVerticalSpace(40),
+            const SizedBox(height: 40),
             const Padding(
               padding: EdgeInsets.only(top: 20.0),
               child: Text(
-                'Profile Page',
+                'Profile',
                 style: TextStyle(fontSize: 24),
               ),
             ),
-            addVerticalSpace(30),
-            ElevatedButton(onPressed: doLogout, child: const Text('Logout')),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                onPressed: () => doPassword(context),
+                style: ElevatedButton.styleFrom(fixedSize: const Size(180, 30)),
+                child: const Text('Change Password')),
+            const SizedBox(height: 30),
+            ElevatedButton(
+                onPressed: doLogout,
+                style: ElevatedButton.styleFrom(fixedSize: const Size(180, 30)),
+                child: const Text('Logout')),
           ],
         ),
       ),
@@ -53,7 +62,7 @@ class Profile extends StatelessWidget {
         currentIndex: 1,
         onTap: (index) {
           if (index == 0) {
-            Get.to(HomePage());
+            Get.to(const HomePage());
           }
         },
       ),
@@ -62,5 +71,59 @@ class Profile extends StatelessWidget {
 
   void doLogout() {
     Get.find<AuthCtl>().auth.signOut();
+  }
+
+  void doPassword(context) {
+    TextEditingController emailCtl = TextEditingController();
+
+    SimpleDialog dialog = SimpleDialog(
+      elevation: 10,
+      shadowColor: Colors.black,
+      title: const Text("Reset password", style: TextStyle(fontSize: 18)),
+      backgroundColor: Colors.yellow[100],
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: 'E-mail address'),
+                  controller: emailCtl,
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: Icon(Icons.send),
+                      onPressed: () => sendPasswordCode(emailCtl.text),
+                      label: Text("Send me a code"),
+                    ),
+                  ],
+                )
+              ],
+            )),
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return dialog;
+      },
+    );
+  }
+
+  void sendPasswordCode(email) {
+    final bool emailValid = GetUtils.isEmail(email);
+    if (!emailValid) {
+      Get.snackbar("Invalid Email", "You must be supply a valid email address",
+          snackPosition: SnackPosition.BOTTOM, duration: const Duration(seconds: 5));
+      return;
+    }
+    Get.find<AuthCtl>().auth.sendPasswordResetEmail(email: email);
+    Get.back();
+    Get.snackbar("Check your inbox", "If that email matches your login email, you will receive a password reset code.",
+        snackPosition: SnackPosition.BOTTOM, duration: const Duration(seconds: 5));
   }
 }
