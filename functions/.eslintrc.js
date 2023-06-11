e66553a37acef87564f4844f4eda7ac79e6d6c1e
Will Gilpin <wgilpin@gmail.com>
2023-06-11 23:25:51 +0100

    tidying

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,40 +2,49 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'google',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
-    sourceType: 'module',
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module"
   },
   ignorePatterns: [
-    '/lib/**/*', // Ignore built files.
+    "/lib/**/*" // Ignore built files.
   ],
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    'quote-props': ['error', 'as-needed'],
-    quotes: ['error', 'single'],
-    'import/no-unresolved': 0,
-    indent: ['error', 2],
-    semi: ['error', 'never'],
-    // eslint-disable-next-line object-curly-spacing
-    'max-len': ['error', { code: 120 }],
-    '@typescript-eslint/no-unused-vars': [
-      'warn', // or "error"
+    "quote-props": ["error", "as-needed"],
+    quotes: ["error", "single"],
+    "import/no-unresolved": 0,
+    indent: [
+      "error",
+      2,
       {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
+        CallExpression: {
+          arguments: "first"
+        }
+      }
     ],
-  },
-}
+    "function-call-argument-newline": ["error", "multiline"],
+    semi: ["error", "never"],
+    // eslint-disable-next-line object-curly-spacing
+    "max-len": ["error", {code: 120}],
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_"
+      }
+    ]
+  }
+};
