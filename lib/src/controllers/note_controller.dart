c30847bbcf5a2f9f72348c04cd314b392e18d906
Will Gilpin <wgilpin@gmail.com>
2023-06-28 11:19:02 +0100

    refactor to a notes controller & a single note one

--- lib/src/controllers/note_controller.dart
+++ lib/src/controllers/note_controller.dart
@@ -1,40 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import 'package:keep_app/src/controllers/auth_controller.dart';
 import 'package:keep_app/src/notes.dart';
 
-class NoteController {
-  static final authCtl = Get.find<AuthCtl>();
+class NoteController extends GetxController {
+  NoteController(this.id, this.onLoaded);
 
-  static Stream<QuerySnapshot> getData() {
-    try {
-      debugPrint('========== NoteController.getData');
+  Function() onLoaded;
+  String id;
 
-      final db = FirebaseFirestore.instance;
-      String uid = authCtl.user!.uid;
-      if (uid.isNotEmpty) {
-        return db
-            .collection('notes')
-            .where("user", isEqualTo: db.doc("/users/$uid"))
-            .orderBy("updatedAt", descending: true)
-            .snapshots();
-      } else {
-        return const Stream.empty();
-      }
-    } catch (e) {
-      Get.snackbar("Error Loading", e.toString());
-      debugPrint(e.toString());
-      return const Stream.empty();
-    }
+  final Rx<Note?> _note = Rx<Note?>(null);
+  Note? get note => _note.value;
+  DocumentReference get ref => FirebaseFirestore.instance.collection('notes').doc(id);
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+
+    final snapshot = await ref.get();
+    _note.value = Note.fromSnapshot(snapshot);
+
+    ref.snapshots().listen((snapshot) {
+      _note.value = Note.fromSnapshot(snapshot);
+    });
+    onLoaded();
   }
 
-  static Future<List<Note>> setData(List<String> noteIds) async {
-    final List<Note> notes = [];
-    for (var id in noteIds) {
-      final note = await getNote(id);
-      notes.add(note);
-    }
-    return notes;
+  Future<void> addCheckItem(String title) async {
+    await ref.update({
+      "checklist": FieldValue.arrayUnion([
+        {"title": title, "checked": false}
+      ])
+    });
+    refresh();
+  }
+
+  Future<void> delete(String noteId) async {
+    ref.delete();
+    refresh();
   }
 }
---
+++ lib/src/controllers/notes_controller.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:keep_app/src/controllers/auth_controller.dart';
+import 'package:keep_app/src/controllers/database.dart';
+import 'package:keep_app/src/notes.dart';
+
+class NotesController extends GetxController {
+  static final authCtl = Get.find<AuthCtl>();
+  Rx<List<Note>> notes = Rx<List<Note>>([]);
+  List<Note> get notesList => notes.value;
+  late final CollectionReference notesCollection;
+
+  @override
+  void onInit() {
+    super.onInit();
+    notes.bindStream(Database().noteStream(authCtl.user!.uid));
+    notesCollection = FirebaseFirestore.instance.collection('notes');
+  }
+
+  Note findNoteById(String id) {
+    return notesList.firstWhere((note) => note.id == id);
+  }
+
+  Future<void> updateChecklist(String id, List<CheckItem> checklist) async {
+    await notesCollection.doc(id).update({"checklist": checklist.map((e) => e.toJson()).toList()});
+    refresh();
+  }
+
+  Future<void> updateNoteFromMap(String id, Map<String, Object?> map) async {
+    await notesCollection.doc(id).update(map);
+    refresh();
+  }
+
+  Future<void> createNoteFromMap(Map<String, Object?> note) async {
+    await notesCollection.add(note);
+    refresh();
+  }
+}
