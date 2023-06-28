0f2dd6f61a69858dd34b0bfa536ec46fb13408f1
Will Gilpin <wgilpin@gmail.com>
2023-06-28 11:19:42 +0100

    remove relative imports

--- lib/src/controllers/bindings/app_binding.dart
+++ lib/src/controllers/bindings/app_binding.dart
@@ -1,16 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:keep_app/src/controllers/note_controller.dart';
-
-import '../auth_controller.dart';
+import 'package:keep_app/src/controllers/auth_controller.dart';
+import 'package:keep_app/src/controllers/notes_controller.dart';
 
 class AppBindings extends Bindings {
   /// Bindings are used to inject dependencies into GetX.
   @override
   void dependencies() {
-    Get.put(AuthCtl(FirebaseAuth.instance));
-    Get.put<NoteController>(NoteController());
+    // auth controller singleton
+    Get.put(AuthCtl(FirebaseAuth.instance), permanent: true);
+
+    // note controller singleton
+    Get.put<NotesController>(NotesController(), permanent: true);
     debugPrint('Bindings set');
   }
 }
