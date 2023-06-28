8e669e817f760815d52ce6d0cdb03fe4f82a3cf5
Will Gilpin <wgilpin@gmail.com>
2023-06-28 11:23:03 +0100

    fix race condition on load recommended

--- lib/src/views/recommend.dart
+++ lib/src/views/recommend.dart
@@ -3,7 +3,6 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:keep_app/src/controllers/auth_controller.dart';
-import 'package:keep_app/src/notes.dart';
 
 /// Class to call the recommender cloud functions
 class Recommender {
@@ -19,8 +18,11 @@ class Recommender {
         return {};
       }
       Map<String, String> res = {};
+      debugPrint('noteSearch: $text');
+
       for (var entry in results.data) {
         res[entry["id"]] = entry["title"];
+        debugPrint('noteSearch: ${entry["id"]} ${entry["title"]}');
       }
       return res;
     } on FirebaseFunctionsException catch (e) {
@@ -29,37 +31,37 @@ class Recommender {
     }
   }
 
-  static Future<List<Map<String, String>>> noteSearch(Note note, int count, context) async {
+  static Future<List<Map<String, String>>> noteSearch(String noteId, int count, context) async {
     try {
       final uid = Get.find<AuthCtl>().user!.uid;
       final userSnap = await FirebaseFirestore.instance.collection('users').doc(uid).get();
-      late Timestamp lastUpdated;
-      final data = userSnap.data();
-      if (data != null && data.containsKey('lastUpdated')) {
-        lastUpdated = data['lastUpdated'];
-      } else {
-        // set to epoch 0
-        lastUpdated = Timestamp.fromMicrosecondsSinceEpoch(0);
-      }
-      final relatedUpdated =
-          note.relatedUpdated == null ? Timestamp.fromMicrosecondsSinceEpoch(0) : note.relatedUpdated as Timestamp;
-      if (note.related != null && note.related!.isNotEmpty) {
-        if (lastUpdated.compareTo(relatedUpdated) <= 0.0) {
-          debugPrint('noteSearch cached: U: ${lastUpdated.toDate()} N:${relatedUpdated.toDate()}');
-          return note.related!;
-        }
-      }
-      debugPrint('noteSearch cloud function : U: ${lastUpdated.toDate()} N:${relatedUpdated.toDate()}');
+      // late Timestamp lastUpdated;
+      // final data = userSnap.data();
+      // if (data != null && data.containsKey('lastUpdated')) {
+      //   lastUpdated = data['lastUpdated'];
+      // } else {
+      //   // set to epoch 0
+      //   lastUpdated = Timestamp.fromMicrosecondsSinceEpoch(0);
+      // }
+      // final relatedUpdated =
+      //     note.relatedUpdated == null ? Timestamp.fromMicrosecondsSinceEpoch(0) : note.relatedUpdated as Timestamp;
+      // if (note.related != null && note.related!.isNotEmpty) {
+      //   if (lastUpdated.compareTo(relatedUpdated) <= 0.0) {
+      //     debugPrint('noteSearch cached: U: ${lastUpdated.toDate()} N:${relatedUpdated.toDate()}');
+      //     return note.related!;
+      //   }
+      // }
+      // debugPrint('noteSearch cloud function : U: ${lastUpdated.toDate()} N:${relatedUpdated.toDate()}');
 
       final functions = FirebaseFunctions.instance;
       final callable = functions.httpsCallable('noteSearch');
-      final results = await callable.call({"noteId": note.id, "maxResults": count});
+      final results = await callable.call({"noteId": noteId, "maxResults": count});
       if (results.data == null) {
         return [];
       }
       final List<Map<String, String>> res = [];
       for (var e in results.data) {
-        if (e["id"] != note.id) {
+        if (e["id"] != noteId) {
           res.add({
             "id": e["id"],
             "title": e["title"],
